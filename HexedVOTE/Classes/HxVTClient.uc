hexengraf/HexedUT2k4

commit 30ad0102524fc373004e1235f1ed4e546a86a2a0

Improve the abstraction provided by HxConfig

HexedVOTE/Classes/HxVTClient.uc
@@ -106,7 +106,7 @@ simulated function PopulateMapEntries()
     local int i;
 
     Limit = 8 * MESSAGES_PER_TICK;
-    for (i = Maps.Length; i < VRI.MapList.Length && Limit > 0; ++i)
+    for (i = Maps.Length; i < VRI.MapList.Length; ++i)
     {
         Maps.Insert(Maps.Length, 1);
         Resources.Insert(Resources.Length, 1);
@@ -126,10 +126,14 @@ simulated function PopulateMapEntries()
             Resources[i].PreviewName = Record.ScreenshotRef;
             MapEntryResponseCount += 2;
         }
-        else
+        else if (Limit > 0)
         {
             ServerRequestMapEntry(i);
             --Limit;
+        }
+        else
+        {
+            break;
         }
     }
 }
@@ -305,7 +309,7 @@ simulated function ParseArrayProperty(int Index, array<string> Values)
     }
 }
 
-simulated function ServerPropertiesReady()
+simulated function NotifyServerPropertiesReady()
 {
     class'HxMapVotingPage'.default.VoteListCustomBG = GetServerProperty("VoteListCustomBG");
     class'HxMapVotingPage'.default.MapListCustomBG = GetServerProperty("MapListCustomBG");
@@ -314,7 +318,7 @@ simulated function ServerPropertiesReady()
     UpdateResourcePreviews();
 }
 
-simulated function ServerPropertyChanged(int Index, string OldValue)
+simulated function NotifyServerPropertyChanged(int Index, string OldValue)
 {
     switch (MutatorClass.default.Properties[Index].Name)
     {
@@ -483,7 +487,6 @@ static private final function string GetMapDescriptionFromRecord(CacheManager.Ma
     }
     return Record.Description;
 }
-
 
 defaultproperties
 {

HexedARENA/Classes/HxCTClient.uc
@@ -32,7 +32,7 @@ simulated function bool InitializeClient()
     return false;
 }
 
-simulated function ServerPropertiesReady()
+simulated function NotifyServerPropertiesReady()
 {
     local xPickUpBase PickupBase;
 
@@ -50,7 +50,7 @@ simulated function ServerPropertiesReady()
     }
 }
 
-simulated function ServerPropertyChanged(int Index, string OldValue)
+simulated function NotifyServerPropertyChanged(int Index, string OldValue)
 {
     switch (GetServerPropertyName(Index))
     {

HexedARENA/Classes/HxGUIMenuInstagibPanel.uc
@@ -109,47 +109,42 @@ function InternalOnLoadINI(GUIComponent Sender, string s)
 
 function InternalOnChange(GUIComponent Sender)
 {
-    if (Client != None)
+    switch (Sender)
     {
-        switch (Sender)
-        {
-            case sl_ReticleRedColor:
-                Config.ReticleColor.R = byte(sl_ReticleRedColor.GetComponentValue());
-                break;
-            case sl_ReticleGreenColor:
-                Config.ReticleColor.G = byte(sl_ReticleGreenColor.GetComponentValue());
-                break;
-            case sl_ReticleBlueColor:
-                Config.ReticleColor.B = byte(sl_ReticleBlueColor.GetComponentValue());
-                break;
-            case sl_ReticleOpacity:
-                Config.ReticleColor.A = byte(sl_ReticleOpacity.GetComponentValue());
-                break;
-            case sl_CrosshairRedColor:
-                Config.CustomZoomCrosshairColor.R = byte(sl_CrosshairRedColor.GetComponentValue());
-                break;
-            case sl_CrosshairGreenColor:
-                Config.CustomZoomCrosshairColor.G = byte(sl_CrosshairGreenColor.GetComponentValue());
-                break;
-            case sl_CrosshairBlueColor:
-                Config.CustomZoomCrosshairColor.B = byte(sl_CrosshairBlueColor.GetComponentValue());
-                break;
-            case sl_CrosshairOpacity:
-                Config.CustomZoomCrosshairColor.A = byte(sl_CrosshairOpacity.GetComponentValue());
-                break;
-            default:
-                Client.SetProperty(
-                    Config.Index, Sender.Tag, GUIMenuOption(Sender).GetComponentValue());
-                break;
-        }
-        switch (Config.Properties[Sender.Tag].Name)
-        {
-            case "ReticleColor":
-            case "CustomZoomCrosshairColor":
-                Client.SetProperty(
-                    Config.Index, Sender.Tag, Config.GetProperty(Sender.Tag));
-                break;
-        }
+        case sl_ReticleRedColor:
+            Config.ReticleColor.R = byte(sl_ReticleRedColor.GetComponentValue());
+            break;
+        case sl_ReticleGreenColor:
+            Config.ReticleColor.G = byte(sl_ReticleGreenColor.GetComponentValue());
+            break;
+        case sl_ReticleBlueColor:
+            Config.ReticleColor.B = byte(sl_ReticleBlueColor.GetComponentValue());
+            break;
+        case sl_ReticleOpacity:
+            Config.ReticleColor.A = byte(sl_ReticleOpacity.GetComponentValue());
+            break;
+        case sl_CrosshairRedColor:
+            Config.CustomZoomCrosshairColor.R = byte(sl_CrosshairRedColor.GetComponentValue());
+            break;
+        case sl_CrosshairGreenColor:
+            Config.CustomZoomCrosshairColor.G = byte(sl_CrosshairGreenColor.GetComponentValue());
+            break;
+        case sl_CrosshairBlueColor:
+            Config.CustomZoomCrosshairColor.B = byte(sl_CrosshairBlueColor.GetComponentValue());
+            break;
+        case sl_CrosshairOpacity:
+            Config.CustomZoomCrosshairColor.A = byte(sl_CrosshairOpacity.GetComponentValue());
+            break;
+        default:
+            Config.SetProperty(Sender.Tag, GUIMenuOption(Sender).GetComponentValue());
+            break;
+    }
+    switch (Config.Properties[Sender.Tag].Name)
+    {
+        case "ReticleColor":
+        case "CustomZoomCrosshairColor":
+            Config.SetProperty(Sender.Tag, Config.GetProperty(Sender.Tag));
+            break;
     }
 }
 

HexedARENA/Classes/HxIGClient.uc
@@ -12,29 +12,6 @@ simulated function Tick(float DeltaTime)
     Super.Tick(DeltaTime);
 }
 
-simulated function bool SetProperty(int ConfigIndex, int PropertyIndex, string Value)
-{
-    local PlayerController PC;
-    local Inventory Inv;
-
-    if (Super.SetProperty(ConfigIndex, PropertyIndex, Value))
-    {
-        PC = Level.GetLocalPlayerController();
-        if (PC != None && PC.Pawn != None)
-        {
-            for (Inv = PC.Pawn.Inventory; Inv != None; Inv = Inv.inventory)
-            {
-                if (HxZoomSuperShockRifle(Inv) != None)
-                {
-                    HxZoomSuperShockRifle(Inv).RefreshConfiguration();
-                }
-            }
-        }
-        return true;
-    }
-    return false;
-}
-
 defaultproperties
 {
     MutatorClass=class'MutHexedINSTAGIB'

HexedARENA/Classes/HxZoomSuperShockRifleConfig.uc
@@ -14,7 +14,7 @@ var config Color CustomZoomCrosshairColor;
 var config float CustomZoomCrosshairScale;
 var config string CustomZoomCrosshairTextureName;
 
-function ApplyAllProperties()
+function InitializeProperties()
 {
     class'HxZoomSuperShockRifle'.default.ScopeOverlay = ScopeOverlay;
     class'HxZoomSuperShockRifle'.default.bSoundEffects = bSoundEffects;
@@ -28,6 +28,7 @@ function ApplyAllProperties()
     class'HxZoomSuperShockRifle'.default.CustomZoomCrosshairScale = CustomZoomCrosshairScale;
     class'HxZoomSuperShockRifle'.default.CustomZoomCrosshairTextureName =
         CustomZoomCrosshairTextureName;
+    UpdateDynamicActors();
 }
 
 function ApplyProperty(int Index)
@@ -71,68 +72,98 @@ function ApplyProperty(int Index)
                 CustomZoomCrosshairTextureName;
             break;
     }
+    UpdateDynamicActors();
 }
 
 function bool ResetProperty(int Index)
 {
+    local bool bReset;
+
     switch (Index)
     {
         case 0:
             ScopeOverlay = default.ScopeOverlay;
-            return true;
+            bReset = true;
+            break;
         case 1:
             bSoundEffects = default.bSoundEffects;
-            return true;
+            bReset = true;
+            break;
         case 2:
             bShowChargeBar = default.bShowChargeBar;
-            return true;
+            bReset = true;
+            break;
         case 3:
             ReticleColor = default.ReticleColor;
-            return true;
+            bReset = true;
+            break;
         case 4:
             ReticleScale = default.ReticleScale;
-            return true;
+            bReset = true;
+            break;
         case 5:
             BackgroundOpacity = default.BackgroundOpacity;
-            return true;
+            bReset = true;
+            break;
         case 6:
             bCustomZoomCrosshair = default.bCustomZoomCrosshair;
-            return true;
+            bReset = true;
+            break;
         case 7:
             CustomZoomCrosshair = default.CustomZoomCrosshair;
-            return true;
+            bReset = true;
+            break;
         case 8:
             CustomZoomCrosshairColor = default.CustomZoomCrosshairColor;
-            return true;
+            bReset = true;
+            break;
         case 9:
             CustomZoomCrosshairScale = default.CustomZoomCrosshairScale;
-            return true;
+            bReset = true;
+            break;
         case 10:
             CustomZoomCrosshairTextureName = default.CustomZoomCrosshairTextureName;
-            return true;
+            bReset = true;
+            break;
     }
-    return false;
+    if (bReset)
+    {
+        UpdateDynamicActors();
+    }
+    return bReset;
 }
 
 function bool SetProperty(int Index, coerce string Value)
 {
-    if (Super.SetProperty(Index, Value))
-    {
-        if (Index == 7)
-        {
-            SetCustomCrosshair(Value);
-        }
-        return true;
-    }
-    return false;
-}
-
-function SetCustomCrosshair(coerce int Index)
-{
     local array<CacheManager.CrosshairRecord> Crosshairs;
 
-    class'CacheManager'.static.GetCrosshairList(Crosshairs);
-    SetProperty(10, string(Crosshairs[Index].CrosshairTexture));
+    if (Index == 7)
+    {
+        class'CacheManager'.static.GetCrosshairList(Crosshairs);
+        SetProperty(10, string(Crosshairs[int(ValidateProperty(Index, Value))].CrosshairTexture));
+    }
+    return Super.SetProperty(Index, Value);
+}
+
+function UpdateDynamicActors()
+{
+    local PlayerController PC;
+    local Inventory Inv;
+
+    if (Level != None)
+    {
+        PC = Level.GetLocalPlayerController();
+        if (PC != None && PC.Pawn != None)
+        {
+            for (Inv = PC.Pawn.Inventory; Inv != None; Inv = Inv.inventory)
+            {
+                if (HxZoomSuperShockRifle(Inv) != None)
+                {
+                    HxZoomSuperShockRifle(Inv).RefreshConfiguration();
+                }
+            }
+        }
+    }
 }
 
 defaultproperties

HexedSRC/Classes/HxClientManager.uc
@@ -57,7 +57,7 @@ simulated function bool InitializeClient()
     return PC != None && GC != None;
 }
 
-simulated function NotifyServerInfoChanged(HxClientReplicationInfo Sender)
+simulated function NotifyServerPropertyChanged(HxClientReplicationInfo Sender)
 {
     if (Level.NetMode == NM_Client)
     {
@@ -229,22 +229,45 @@ simulated private function bool TrySetKeybind(string Keybind)
     return false;
 }
 
-simulated function int EncodeTag(int CRIIndex, int PropertyIndex, optional int ConfigIndex)
+simulated function int EncodeTag(int CRINumber, int Index, optional int ConfigNumber)
 {
-    return ((CRIIndex & 0x3ff) << 20) | ((ConfigIndex & 0x3ff) << 10) | (PropertyIndex & 0x3ff);
+    return ((CRINumber & 0x3ff) << 20) | ((ConfigNumber & 0x3ff) << 10) | (Index & 0x3ff);
 }
 
-simulated function bool DecodeTag(int Tag,
-                                  out int CRIIndex,
-                                  out int PropertyIndex,
-                                  optional out int ConfigIndex)
+simulated function int DecodeServerTag(int Tag,
+                                       optional out HxClientReplicationInfo CRI,
+                                       optional out int Index)
 {
+    local int CRINumber;
+
     if (Tag >= 0)
     {
-        PropertyIndex = Tag & 0x3ff;
-        ConfigIndex = (Tag >> 10) & 0x3ff;
-        CRIIndex = (Tag >> 20) & 0x3ff;
-        return CRIs[CRIIndex] != None;
+        Index = Tag & 0x3ff;
+        CRINumber = (Tag >> 20) & 0x3ff;
+        CRI = CRIs[CRINumber];
+        if (CRI != None)
+        {
+            return CRINumber;
+        }
+    }
+    return -1;
+}
+
+simulated function bool DecodeUserTag(int Tag, out HxConfig Config, out int Index)
+{
+    local int CRINumber;
+    local int ConfigNumber;
+
+    if (Tag >= 0)
+    {
+        Index = Tag & 0x3ff;
+        ConfigNumber = (Tag >> 10) & 0x3ff;
+        CRINumber = (Tag >> 20) & 0x3ff;
+        if (CRIs[CRINumber] != None)
+        {
+            Config = CRIs[CRINumber].Configs[ConfigNumber];
+            return Config != None;
+        }
     }
     return false;
 }

HexedSRC/Classes/HxClientReplicationInfo.uc
@@ -46,8 +46,9 @@ replication
         ServerUpdateProperty;
 }
 
-simulated function ServerPropertiesReady();
-simulated function ServerPropertyChanged(int Index, string OldValue);
+simulated function NotifyServerPropertiesReady();
+simulated function NotifyServerPropertyChanged(int Index, string OldValue);
+simulated function NotifyUserPropertyChanged(HxConfig Config, int Index, string OldValue);
 simulated function ParseArrayProperty(int Index, array<string> Values);
 simulated function ReceiveCustomMessage(HxReplicationMessage Message);
 
@@ -64,7 +65,7 @@ simulated event PreBeginPlay()
         for (i = 0; i < ConfigClasses.Length; ++i)
         {
             Configs[i] = Manager.LoadConfig(ConfigClasses[i]);
-            Configs[i].Index = i;
+            Configs[i].Setup(Self);
         }
         Manager.Register(Self);
     }
@@ -89,7 +90,7 @@ simulated event Tick(float DeltaTime)
         else
         {
             bServerPropertiesReady = true;
-            ServerPropertiesReady();
+            NotifyServerPropertiesReady();
         }
         bServerPropertiesRequested = true;
     }
@@ -182,14 +183,14 @@ simulated function ClientReceiveMessage(HxReplicationMessage Message)
             }
             if (bServerPropertiesReady)
             {
-                ServerPropertyChanged(Message.Index, OldValue);
+                NotifyServerPropertyChanged(Message.Index, OldValue);
             }
             else if (Message.Index == ServerInfo.Settings.Length - 1)
             {
                 bServerPropertiesReady = true;
-                ServerPropertiesReady();
+                NotifyServerPropertiesReady();
             }
-            Manager.NotifyServerInfoChanged(Self);
+            Manager.NotifyServerPropertyChanged(Self);
             break;
         case HX_RMSG_ArrayElement:
             ReplicatedArrayProperty[ReplicatedArrayProperty.Length] = Message.Value;
@@ -226,25 +227,6 @@ simulated function string GetServerPropertyName(int Index)
 simulated function ClientOpenConfigurationMenu()
 {
     Manager.OpenConfigurationMenu(Self);
-}
-
-simulated function string GetProperty(int ConfigIndex, int PropertyIndex)
-{
-    if (IsValidConfigIndex(ConfigIndex))
-    {
-        return Configs[ConfigIndex].GetProperty(PropertyIndex);
-    }
-    return "";
-}
-
-simulated function bool SetProperty(int ConfigIndex, int PropertyIndex, string Value)
-{
-    if (IsValidConfigIndex(ConfigIndex) && Configs[ConfigIndex].SetProperty(PropertyIndex, Value))
-    {
-        Configs[ConfigIndex].SaveConfig();
-        return true;
-    }
-    return false;
 }
 
 simulated final function HxConfig FindConfig(class<HxConfig> ConfigClass)

HexedSRC/Classes/HxConfig.uc
@@ -4,9 +4,11 @@ class HxConfig extends HxTypes
 var const string ObjectName;
 var const array<HxProperty> Properties;
 var const array<HxDisplayProperty> DisplayInfo;
-var int Index;
 
-function ApplyAllProperties();
+var protected LevelInfo Level;
+var protected HxClientReplicationInfo ClientOwner;
+
+function InitializeProperties();
 function ApplyProperty(int Index);
 function string ValidateStruct(int Index, string Value);
 
@@ -24,16 +26,32 @@ function Created()
         }
     }
     SaveConfig();
-    ApplyAllProperties();
+}
+
+function Setup(HxClientReplicationInfo Owner)
+{
+    Level = Owner.Level;
+    ClientOwner = Owner;
+    InitializeProperties();
 }
 
 function bool SetProperty(int Index, coerce string Value)
 {
-    if (IsValidPropertyIndex(Index)
-        && SetPropertyText(Properties[Index].Name, ValidateProperty(Index, Value)))
+    local string OldValue;
+
+    if (IsValidPropertyIndex(Index))
     {
-        ApplyProperty(Index);
-        return true;
+        OldValue = GetPropertyText(Properties[Index].Name);
+        if (SetPropertyText(Properties[Index].Name, ValidateProperty(Index, Value)))
+        {
+            ApplyProperty(Index);
+            if (ClientOwner != None)
+            {
+                ClientOwner.NotifyUserPropertyChanged(Self, Index, OldValue);
+            }
+            SaveConfig();
+            return true;
+        }
     }
     return false;
 }
@@ -125,6 +143,29 @@ function UpdateConfiguration(Object TargetObject)
     }
 }
 
+function HudOverlay FindHudOverlay(class<HudOverlay> OverlayClass)
+{
+    local PlayerController PC;
+    local int i;
+
+    if (Level != None)
+    {
+        PC = Level.GetLocalPlayerController();
+        if (PC != None && PC.MyHUD != None)
+        {
+            for (i = 0; i < PC.MyHUD.Overlays.Length; ++i)
+            {
+                if (PC.MyHUD.Overlays[i].Class == OverlayClass)
+                {
+                    return PC.MyHUD.Overlays[i];
+                }
+            }
+        }
+    }
+    return None;
+}
+
+
 static function HxConfig Load()
 {
     return new(None, default.ObjectName) default.Class;
@@ -132,5 +173,4 @@ static function HxConfig Load()
 
 defaultproperties
 {
-    Index=-1
 }

commit fef1477c58aeff0475a7c5ea5760203eba491de9

Fix bug introduced in 30ad010 deadlocking map info replication
The break point due to insufficient limit must be done before allocating
a new map entry.

HexedVOTE/Classes/HxVTClient.uc
@@ -108,13 +108,17 @@ simulated function PopulateMapEntries()
     Limit = 2 * MESSAGES_PER_TICK;
     for (i = Maps.Length; i < VRI.MapList.Length; ++i)
     {
+        Record = class'CacheManager'.static.GetMapRecord(VRI.MapList[i].MapName);
+        if (Record.MapName == "" && Limit == 0)
+        {
+            break;
+        }
         Maps.Insert(Maps.Length, 1);
         Resources.Insert(Resources.Length, 1);
         Maps[i].Name = VRI.MapList[i].MapName;
         Maps[i].Played = VRI.MapList[i].PlayCount;
         Maps[i].Sequence = VRI.MapList[i].Sequence;
         Maps[i].Tag = Favorites.Get(VRI.MapList[i].MapName);
-        Record = class'CacheManager'.static.GetMapRecord(VRI.MapList[i].MapName);
         if (Record.MapName != "")
         {
             Maps[i].Label = Record.FriendlyName;
@@ -126,14 +130,10 @@ simulated function PopulateMapEntries()
             Resources[i].PreviewName = Record.ScreenshotRef;
             MapEntryResponseCount += 2;
         }
-        else if (Limit > 0)
+        else
         {
             ServerRequestMapEntry(i);
             --Limit;
-        }
-        else
-        {
-            break;
         }
     }
 }